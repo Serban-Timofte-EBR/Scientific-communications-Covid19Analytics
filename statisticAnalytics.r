46355d7ebc61da32a3236d7924bd9ec93a1564a1
Timofte Serban Gabriel <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2023-03-16 22:11:19 +0200

    Update statisticAnalytics.r

--- statisticAnalytics.r
+++ statisticAnalytics.r
@@ -1,35 +1,32 @@
-rm(list=ls()) # removes all variables stored previously
-library(Hmisc) # import
+rm(list=ls()) 
+library(Hmisc)
 
 data <- read.csv("C:/Users/mykyt/Desktop/covid_R/COVID19_line_list_data.csv")
-describe(data) # Hmisc command
+describe(data)
 
-# cleaned up death column
+# cleaned up data
 data$death_dummy <- as.integer(data$death != 0)
-# death rate
-sum(data$death_dummy) / nrow(data)
+sum(data$death_dummy) / nrow(data) # mortalitate = 0.058064
 
-# AGE
-# claim: people who die are older
+# Varsta
+# H0: oamenii care au murit sunt mai batrani
 dead = subset(data, death_dummy == 1)
 alive = subset(data, death_dummy == 0)
 mean(dead$age, na.rm = TRUE)
 mean(alive$age, na.rm = TRUE)
-# is this statistically significant?
+# Statistic significat?
 t.test(alive$age, dead$age, alternative="two.sided", conf.level = 0.99)
-# normally, if p-value < 0.05, we reject null hypothesis
-# here, p-value ~ 0, so we reject the null hypothesis and 
-# conclude that this is statistically significant
+# diferenta dintre o persoana care moare si una care traieste este in intervalul -25.52122 si -15.50661
+# p-value is 2.2e-16 ~ 0 < 0.05, deci oamenii care au murit de Covid 19 sunt mai batrani
 
-# GENDER
-# claim: gender has no effect
+
+# Gen
+# H0: genul nu are efect
 men = subset(data, gender == "male")
 women = subset(data, gender == "female")
 mean(men$death_dummy, na.rm = TRUE) #8.5%!
 mean(women$death_dummy, na.rm = TRUE) #3.7%
-# is this statistically significant?
+# Statistic significat?
 t.test(men$death_dummy, women$death_dummy, alternative="two.sided", conf.level = 0.99)
-# 99% confidence: men have from 0.8% to 8.8% higher chance
-# of dying.
-# p-value = 0.002 < 0.05, so this is statistically
-# significant
+# Barbatii au cu 0,8% pana la 8.8% sanse mai mari de mortalitate in cazul infectarii cu Covid19
+# p-value = 0.002 < 0.05, semnificativ statistic
